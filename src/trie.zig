6da02edf787c0be65e7d40aae0412a4baded3427
Clinton Nmereole <clintonnmereole@gmail.com>
2024-02-24 22:02:54 -0600

    my first commit
    this is the first commit of this repository

---
+++ src/trie.zig
@@ -0,0 +1,54 @@
+const std = @import("std");
+const print = std.debug.print;
+var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+const gpa_allocator = gpa.allocator();
+
+const TrieNode = struct {
+    children: [26]?*TrieNode = [_]?*TrieNode{null} ** 26,
+    is_word: bool = false,
+};
+
+const Trie = struct {
+    const Self = @This();
+    root: ?*TrieNode = null,
+
+    pub fn charToIndex(char: u8) usize {
+        return char - 'a';
+    }
+
+    pub fn insert(self: *Self, word: []const u8, allocator: std.mem.Allocator) !void {
+        if (self.root == null) {
+            self.root = try allocator.create(TrieNode);
+            self.root.?.* = TrieNode{};
+        }
+        var node = self.root;
+        for (word) |char| {
+            if (node.?.children[charToIndex(char)] == null) {
+                var new_node = try allocator.create(TrieNode);
+                new_node.* = TrieNode{};
+                node.?.children[charToIndex(char)] = new_node;
+            }
+            node = node.?.children[charToIndex(char)].?;
+        }
+        node.?.is_word = true;
+    }
+
+    pub fn search(self: *Self, word: []const u8) bool {
+        if (self.root == null) return false;
+        var node = self.root;
+        for (word) |char| {
+            if (node.?.children[charToIndex(char)] == null) return false;
+            node = node.?.children[charToIndex(char)].?;
+        }
+        return node.?.is_word;
+    }
+};
+
+pub fn main() !void {
+    var trie = Trie{ .root = null };
+    try trie.insert("hello", gpa_allocator);
+    try trie.insert("hi", gpa_allocator);
+    var word: []const u8 = "cat";
+    //try trie.insert("cat", gpa_allocator);
+    print("search for the word {s}: {}\n", .{ word, trie.search(word) });
+}
---
+++ src/wordle.zig
@@ -0,0 +1,14 @@
+const std = @import("std");
+const print = std.debug.print;
+
+const wordle_grid = [5][5]?u8{
+    .{null} ** 5,
+    .{null} ** 5,
+    .{null} ** 5,
+    .{null} ** 5,
+    .{null} ** 5,
+};
+
+pub fn main() void {
+    print("This is the wordle grid: {any}", .{&wordle_grid[0]});
+}
