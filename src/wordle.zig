8d361fb2e0253831e5308bab84277b702b51f758
Clinton Nmereole <clintonnmereole@gmail.com>
2024-02-27 10:26:31 -0600

    Implemented basic wordle game functionality
    Made minimum wordle game that is independent of dictionary. It still needs to be able to search the dictionary to see if the words entered are legit words or just a collection of random characters.

--- src/wordle.zig
+++ src/wordle.zig
@@ -5,20 +5,31 @@ const Trie = @import("trie.zig").Trie;
 var gpa = std.heap.GeneralPurposeAllocator(.{}){};
 const gpa_allocator = gpa.allocator();
 
-const color = enum {
+//helper function
+
+fn valueInArray(value: u8, array: []const u8) bool {
+    for (array) |num| {
+        if (value == num) {
+            return true;
+        }
+    }
+    return false;
+}
+
+pub const color = enum {
     green,
     yellow,
     gray,
 };
 
-const Cell = struct {
+pub const Cell = struct {
     value: ?u8 = null,
     color: ?color = null,
 };
 
-var winning_word: [5]u8 = undefined;
+pub var winning_word: *const [5]u8 = "sunny";
 
-const wordle_grid = [6][5]Cell{
+pub var wordle_grid = [6][5]Cell{
     .{Cell{}} ** 5,
     .{Cell{}} ** 5,
     .{Cell{}} ** 5,
@@ -27,9 +38,8 @@ const wordle_grid = [6][5]Cell{
     .{Cell{}} ** 5,
 };
 
-pub fn main() !void {
-    var trie = Trie{ .root = null };
-    print("This is the wordle grid: {any}\n", .{&wordle_grid[0]});
+pub fn load_dictionary() !Trie {
+    var trie = Trie{};
     const file = try fs.cwd().openFile("dictionary.txt", .{});
     defer file.close();
 
@@ -43,5 +53,46 @@ pub fn main() !void {
             try trie.insert(line, gpa_allocator);
         }
     }
-    print("Search for the word bat: {}\n", .{trie.search("bat")});
+    return trie;
+}
+
+pub fn make_guesses() ![]u8 {
+    //var n: usize = 0;
+    var reader = std.io.getStdIn().reader();
+    var buf: [6]u8 = undefined;
+    var user_guess = try reader.readUntilDelimiter(&buf, '\n');
+    //print("Your guess: {s}\n", .{user_guess});
+    return user_guess;
+}
+
+pub fn start_game() !void {
+    //var trie = try load_dictionary();
+    var n: usize = 0;
+    while (n < 6) : (n += 1) {
+        print("Make guess {d}: ", .{n + 1});
+        var guess = try make_guesses();
+        if (std.mem.eql(u8, guess, winning_word)) {
+            print("You win!\n", .{});
+            break;
+        } else {
+            if (guess.len != 5) {
+                print("Invalid guess\n", .{});
+            } else {
+                for (guess, 0..5) |char, i| {
+                    if (guess[i] == winning_word[i]) {
+                        wordle_grid[n][i] = Cell{ .value = char, .color = color.green };
+                    } else if (valueInArray(guess[i], winning_word)) {
+                        wordle_grid[n][i] = Cell{ .value = char, .color = color.yellow };
+                    } else {
+                        wordle_grid[n][i] = Cell{ .value = char, .color = color.gray };
+                    }
+                }
+            }
+        }
+    }
+    print("This is the wordle grid: {any}\n", .{wordle_grid});
+}
+
+pub fn main() !void {
+    try start_game();
 }
